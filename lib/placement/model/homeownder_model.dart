116da1d78adb7f75b1fc44f64a1f8b3080640e14
gcwill70 <gcwill70@gmail.com>
2023-07-26 11:47:32 -0400

    add additional fields to subclasses

--- lib/placement/model/homeownder_model.dart
+++ lib/placement/model/homeownder_model.dart
@@ -3,9 +3,31 @@ import 'package:pearl/placement/model/vector_model.dart';
 
 @immutable
 class HomeownerModel extends VectorModel {
+  final String name;
+  final List<String> preferences;
+
   const HomeownerModel(
     super.energy,
     super.water,
     super.resilience,
+    this.name,
+    this.preferences,
   );
+
+  @override
+  HomeownerModel copyWith({
+    int? energy,
+    int? water,
+    int? resilience,
+    String? name,
+    List<String>? preferences,
+  }) {
+    return HomeownerModel(
+      energy ?? this.energy,
+      water ?? this.water,
+      resilience ?? this.resilience,
+      name ?? this.name,
+      preferences ?? this.preferences,
+    );
+  }
 }
--- lib/placement/model/neighborhood_model.dart
+++ lib/placement/model/neighborhood_model.dart
@@ -3,9 +3,28 @@ import 'package:pearl/placement/model/vector_model.dart';
 
 @immutable
 class NeighborhoodModel extends VectorModel {
+  final String name;
+
   const NeighborhoodModel(
     super.energy,
     super.water,
     super.resilience,
+    this.name,
   );
+
+  @override
+  NeighborhoodModel copyWith({
+    int? energy,
+    int? water,
+    int? resilience,
+    String? name,
+    List<String>? preferences,
+  }) {
+    return NeighborhoodModel(
+      energy ?? this.energy,
+      water ?? this.water,
+      resilience ?? this.resilience,
+      name ?? this.name,
+    );
+  }
 }
