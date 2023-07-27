947eda85c378324418813a309cc9fd9b14969c15
gcwill70 <gcwill70@gmail.com>
2023-07-27 09:40:52 -0400

    add load test button

--- lib/placement/ui/placement_form.dart
+++ lib/placement/ui/placement_form.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pearl/assets/assets_repo.dart';
 import 'package:pearl/placement/bloc/placement_bloc.dart';
 import 'package:pearl/placement/ui/placement_output_page.dart';
 
@@ -22,8 +21,8 @@ class _PlacementFormState extends State<PlacementForm> {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         SizedBox(
-          width: min(500, MediaQuery.of(context).size.width),
-          height: min(600, MediaQuery.of(context).size.height),
+          width: 500,
+          height: 600,
           child: TextField(
             maxLines: null,
             expands: true,
@@ -36,14 +35,30 @@ class _PlacementFormState extends State<PlacementForm> {
           ),
         ),
         const SizedBox(height: 20),
-        ElevatedButton.icon(
-          icon: const Icon(Icons.home),
-          label: const Text('Place Homeowners'),
-          onPressed: () {
-            final bloc = BlocProvider.of<PlacementBloc>(context);
-            bloc.add(PlacementLoadInput(_controller.text));
-            Navigator.push(context, PlacementOutputPage.route(bloc));
-          },
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton.icon(
+              icon: const Icon(Icons.science),
+              label: const Text('Load Test'),
+              onPressed: () {
+                setState(() {
+                  final input =
+                      RepositoryProvider.of<AssetsRepo>(context).getInputFile();
+                  _controller.value = _controller.value.copyWith(text: input);
+                });
+              },
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(Icons.home),
+              label: const Text('Place Homeowners'),
+              onPressed: () {
+                final bloc = BlocProvider.of<PlacementBloc>(context);
+                bloc.add(PlacementLoadInput(_controller.text));
+                Navigator.push(context, PlacementOutputPage.route(bloc));
+              },
+            ),
+          ],
         ),
       ],
     );
