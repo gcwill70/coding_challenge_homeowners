112fd088ec60f9d681efa444c220001b36b6d8c8
gcwill70 <gcwill70@gmail.com>
2023-07-27 08:55:24 -0400

    add convenience method

--- lib/placement/model/placement.dart
+++ lib/placement/model/placement.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:pearl/placement/placement.dart';
 
-@immutable
-class Placement {
+class Placement extends Equatable {
   final String name;
   final int fit;
 
@@ -18,5 +18,25 @@ class Placement {
       name ?? this.name,
       fit ?? this.fit,
     );
+  }
+
+  static Placement place(
+    HomeownerModel homeowner,
+    NeighborhoodModel neighborhood,
+  ) {
+    return Placement(
+      homeowner.name,
+      homeowner.energy * neighborhood.energy +
+          homeowner.water * neighborhood.water +
+          homeowner.resilience * neighborhood.resilience,
+    );
+  }
+
+  @override
+  List<Object?> get props => [name, fit];
+
+  @override
+  String toString() {
+    return '$name($fit)';
   }
 }
--- lib/placement/placement.dart
+++ lib/placement/placement.dart
@@ -1,6 +1,7 @@
 export 'bloc/placement_bloc.dart';
 export 'model/homeowner_model.dart';
 export 'model/neighborhood_model.dart';
+export 'model/placement.dart';
 export 'model/placement_input.dart';
 export 'model/placement_output.dart';
 export 'model/vector_model.dart';
